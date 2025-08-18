6ff261aecde4a2c2bde4e9aecca26e43b426cc74
Sergey <sergey.soto@gmail.com>
2025-08-18 22:47:50 +0200

    arreglando get_input con varios argumentos. refactorizacion necesaria. luego continuar con el control de errores de las funciones de parseo. quedamos con la funcion is_int_in_range

--- push_swap.c
+++ push_swap.c
@@ -41,25 +41,30 @@ char	**get_input(int	argc, char **argv)
 {
 	int		i;
 	char	**num_array;
-	char	*array_temp;
-	char	*point_temp;
+	char	*joined_args;
+	char	*temp;
+
+	joined_args = NULL;
 	if (argc == 1)
 		return (NULL);
-	else if (argc == 2)
-		array_temp = argv[1];
+	else if (argc >= 2)
+		joined_args = ft_strdup(argv[1]);
 	i = 2;
 	while(i < argc)
 	{
-		point_temp = array_temp;
-		array_temp = ft_strjoin(array_temp, argv[i]);
-		free(point_temp);
-		point_temp = array_temp;
-		array_temp = ft_strjoin(array_temp, " ");
-		free(point_temp);
+		temp = joined_args;
+		joined_args = ft_strjoin(joined_args, " ");
+		free(temp);
+		temp = joined_args;
+		joined_args = ft_strjoin(temp, argv[i]);
+		free(temp);
+		temp = joined_args;
+		joined_args = ft_strjoin(temp, " ");
+		free(temp);
 		i++;
 	}
-	num_array = ft_split(array_temp, ' ');
-	//free(array_temp);
+	num_array = ft_split(joined_args, ' ');
+	free(joined_args);
 	return (num_array);
 }
 
@@ -80,6 +85,7 @@ int	*parse_array(char **array, int *len)
 		if (!is_number_valid(array[i]))
 		{
 			free_int(num_list);
+			free_array(array);
 			printf_error();
 			return (NULL);
 		}
@@ -133,20 +139,23 @@ int	main(int argc, char **argv)
 		free_array(arr);
 		return (0);
 	}
+	ft_putstr_fd("Este es el array:\n", 2);
 	print_array(arr);
 	int *list = parse_array(arr, &len);
 	if (!list)
 	{
-		free_array(arr);
-		free_int(list);
+		// free_array(arr);
+		// free_int(list);
 		return (0);
 	}
+	ft_putstr_fd("Esta es la lista de int:\n", 2);
 	print_list(len, list);
 	printf("la longitud: %d\n", len);
 	if (!parse_list(list, &len))
 	{
 		free_array(arr);
 		free_int(list);
+		printf_error();
 		return (0);
 	}
 	free_int(list);
