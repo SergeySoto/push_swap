caab78ccc6697c249afe7058b2e0f9b8574b6087
ssoto-su <ssoto-su@student.42malaga.com>
2025-09-23 19:03:51 +0200

    Muestra error cuando no le pasan argumentos

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ssoto-su <ssoto-su@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 16:02:27 by ssoto-su          #+#    #+#             */
-/*   Updated: 2025/09/23 17:02:52 by ssoto-su         ###   ########.fr       */
+/*   Updated: 2025/09/23 18:53:59 by ssoto-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-
+	if (argc == 1)
+		return (0);
 	create_stack(argc, argv, &stack_a, &stack_b);
 	sort_numbers(&stack_a, &stack_b);
 	free_stack(stack_a);
