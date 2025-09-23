5d4eb6d36d1295690682d0a3b7ece879e1892bd4
ssoto-su <ssoto-su@student.42malaga.com>
2025-09-23 19:06:10 +0200

    Correcion NO HACE NADA cuando no le pasan argumentos

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ssoto-su <ssoto-su@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 16:02:27 by ssoto-su          #+#    #+#             */
-/*   Updated: 2025/09/23 18:53:59 by ssoto-su         ###   ########.fr       */
+/*   Updated: 2025/09/23 19:05:56 by ssoto-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
+
 	if (argc == 1)
 		return (0);
 	create_stack(argc, argv, &stack_a, &stack_b);
